9fa896746342beb1da5f3c3ed304e51c59065142
LucaLF-dev <laficaraluca@gmail.com>
2023-10-12 17:43:50 +0200

    add - js

--- README.md
+++ README.md
@@ -5,9 +5,7 @@
   - converto la stringa della variabile parola in minuscolo
 - creare una funzione per capire se la parola inserita è palindroma
   - creo una funzione:
-    - creo una variabile   parola rovesciata 
-    - SE la parola è uguale alla parola rovescita return la parola è palindroma
-    - ALTRIMENTI return non è palindroma
+   
 
 # PARI E DISPARI 
 
--- js/app.js
+++ js/app.js
@@ -8,6 +8,39 @@ console.log(parola)
 
 // - creare una funzione per capire se la parola inserita è palindroma
 //   - creo una funzione:
-//     - creo una variabile   parola rovesciata 
-//     - SE la parola è uguale alla parola rovescita return la parola è palindroma
-//     - ALTRIMENTI return non è palindroma
+function checkPalindromo (parola) {
+
+let isPalindrome = true;
+for (let i = 0; i < parola.length / 2; i++) {
+  const charFromBeginnig = parola[i];
+  const charFromEnd = charFromBeginnig[parola.length - 1 - i];
+  if (charFromBeginnig !== charFromEnd) {
+    isPalindrome = false;
+  }
+}
+return isPalindrome;    
+}
+
+// # PARI O DISPARI
+
+// - chiediamo all'utente di scegliere pari o dispari 
+const utentePariODispari = prompt(' scegli pari o di');
+console.log(utentePariODispari);
+// - chiediamo all'utente di inserire un numero da 1 a 5
+const utenteNum = parseInt(prompt('inserisci un numero da 1 a 5'));
+console.log(utenteNum);
+// - generiamo un numero random per il computer da 1 a 5
+function getRandomNumber() {
+    return parseInt((Math.random() * (5-1) + 1));
+}
+const pcNumero = getRandomNumber();
+console.log(pcNumero)
+// - stabiliamo se la somma dei numeri è pari o dispari
+const SommaNumeri = utenteNum + pcNumero;
+console.log(SommaNumeri)
+
+function PariODispari(SommaNumeri) {
+    
+}
+// - dichiariamo chi è il vincitore
+
